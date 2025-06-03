3c15e1eca17df890ba23d6ccd098f1bab15401b7
Reza Goljani <rezagoljani@gmail.com>
2025-06-03 14:00:32 -0700

    Force update to GitHub Pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -6,160 +6,121 @@ const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports =  (env, options)=> {
+module.exports = (env, options) => {
+  const devMode = options.mode === 'development';
 
-    const devMode = options.mode === 'development' ? true : false;
+  process.env.NODE_ENV = options.mode;
 
-    process.env.NODE_ENV = options.mode;
-
-    return {
-        mode: options.mode,
-        entry: path.resolve(__dirname, './src/index.tsx'),
-        output: {
-            path: path.resolve(__dirname, './docs'),
-            filename: '[name].[contenthash].js',
-            chunkFilename: '[name].[contenthash].js',
-            clean: true
+  return {
+    mode: options.mode,
+    entry: path.resolve(__dirname, './src/index.tsx'),
+    output: {
+      path: path.resolve(__dirname, './dist'),  // ← changed from ./docs to ./dist
+      filename: '[name].[contenthash].js',
+      chunkFilename: '[name].[contenthash].js',
+      clean: true,
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components/'),
+        '@constants': path.resolve(__dirname, 'src/constants/'),
+        '@hooks': path.resolve(__dirname, 'src/hooks/'),
+        '@services': path.resolve(__dirname, 'src/services/'),
+        '@store': path.resolve(__dirname, 'src/store/'),
+        '@styles': path.resolve(__dirname, 'src/styles/'),
+        '@utils': path.resolve(__dirname, 'src/utils/'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          loader: 'babel-loader',
         },
-        devtool: 'source-map',
-        resolve: {
-            extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-            alias: {
-                '@components': path.resolve(__dirname, 'src/components/'),
-                '@constants': path.resolve(__dirname, 'src/constants/'),
-                '@hooks': path.resolve(__dirname, 'src/hooks/'),
-                '@services': path.resolve(__dirname, 'src/services/'),
-                '@store': path.resolve(__dirname, 'src/store/'),
-                '@styles': path.resolve(__dirname, 'src/styles/'),
-                '@utils': path.resolve(__dirname, 'src/utils/'),
+        {
+          test: /\.css$/i,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+              },
             },
+            'postcss-loader',
+          ],
         },
-        module: {
-            rules: [
-                {
-                    test: /\.(ts|tsx)$/,
-                    loader: 'babel-loader'
-                },
-                {
-                    test: /\.css$/i,
-                    // include: path.resolve(__dirname, 'src'),
-                    use: [
-                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                        {
-                            loader: "css-loader", 
-                            options: {
-                                sourceMap: true
-                            }
-                        }, 
-                        {
-                            loader: 'postcss-loader'
-                        }
-                    ],
-                },
-                // { 
-                //     test: /\.(woff|woff2|ttf|eot)$/,  
-                //     loader: "file-loader",
-                //     options: {
-                //         name: '[name].[contenthash].[ext]',
-                //     }
-                // },
-                {
-                    test: /\.(woff|woff2|ttf|eot)$/,
-                    type: 'asset/resource',
-                },
-                // { 
-                //     test: /\.(png|jpg|gif|svg)$/,  
-                //     loader: "file-loader",
-                //     options: {
-                //         name: '[name].[contenthash].[ext]',
-                //     }
-                // },
-                {
-                    test: /\.(png|jpg|gif|svg)$/,
-                    type: 'asset/resource',
-                },
-            ]
+        {
+          test: /\.(woff|woff2|ttf|eot)$/,
+          type: 'asset/resource',
         },
-        plugins: [
-            // need to use ForkTsCheckerWebpackPlugin because Babel loader ignores the compilation errors for Typescript
-            new ForkTsCheckerWebpackPlugin(),
-            new MiniCssExtractPlugin({
-                // Options similar to the same options in webpackOptions.output
-                // both options are optional
-                filename: devMode ? '[name].css' : '[name].[contenthash].css',
-                chunkFilename: devMode ? '[name].css' : '[name].[contenthash].css',
-            }),
-            // copy static files from public folder to build directory
-            new CopyPlugin({
-                patterns: [
-                    { 
-                        from: "public/**/*", 
-                        globOptions: {
-                            ignore: ["**/index.html"],
-                        },
-                    }
-                ],
-            }),
-            new HtmlWebpackPlugin({
-                template: './public/index.html',
-                filename: 'index.html',
-                title: package.title,
-                meta: {
-                    title: package.title,
-                    description: package.description,
-                    author: package.author,
-                    keywords: Array.isArray(package.keywords) 
-                        ? package.keywords.join(',') 
-                        : undefined,
-                    'og:title': package.name,
-                    'og:description': package.description,
-                    'og:url': package.homepage,
-                },
-                minify: {
-                    html5                          : true,
-                    collapseWhitespace             : true,
-                    minifyCSS                      : true,
-                    minifyJS                       : true,
-                    minifyURLs                     : false,
-                    removeComments                 : true,
-                    removeEmptyAttributes          : true,
-                    removeOptionalTags             : true,
-                    removeRedundantAttributes      : true,
-                    removeScriptTypeAttributes     : true,
-                    removeStyleLinkTypeAttributese : true,
-                    useShortDoctype                : true
-                }
-            }),
-            // !devMode ? new CleanWebpackPlugin() : false,
-            // !devMode ? new BundleAnalyzerPlugin() : false
-        ].filter(Boolean),
-        optimization: {
-            // splitChunks: {
-            //     cacheGroups: {
-            //         // vendor chunk
-            //         vendor: {
-            //             // sync + async chunks
-            //             chunks: 'all',
-            //             name: 'vendor',
-            //             // import file path containing node_modules
-            //             test: /node_modules/
-            //         }
-            //     }
-            // },
-            minimizer: [
-                new TerserPlugin({
-                    extractComments: true,
-                    terserOptions: {
-                        compress: {
-                            drop_console: true,
-                        }
-                    }
-                }), 
-                new CssMinimizerPlugin()
-            ]
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          type: 'asset/resource',
         },
-    }
-
+      ],
+    },
+    plugins: [
+      new ForkTsCheckerWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[contenthash].css',
+        chunkFilename: devMode ? '[name].css' : '[name].[contenthash].css',
+      }),
+      new CopyPlugin({
+  patterns: [
+    {
+      from: "public/**/*",
+      globOptions: {
+        ignore: ["**/index.html", "**/*.tif"], // <-- exclude .tif files
+      },
+    },
+  ],
+}),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html',
+        title: package.title,
+        meta: {
+          title: package.title,
+          description: package.description,
+          author: package.author,
+          keywords: Array.isArray(package.keywords)
+            ? package.keywords.join(',')
+            : undefined,
+          'og:title': package.name,
+          'og:description': package.description,
+          'og:url': package.homepage,
+        },
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributese: true,
+          useShortDoctype: true,
+        },
+      }),
+    ].filter(Boolean),
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          extractComments: true,
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            },
+          },
+        }),
+        new CssMinimizerPlugin(),
+      ],
+    },
+  };
 };
